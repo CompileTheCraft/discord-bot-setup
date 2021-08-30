0728eb5c4704f88c8dea99f179f671b0bd2afde5
Heatwave Eli <drcharan2007@gmail.com>
2021-08-30 11:16:09 +0530

    update for bot's status

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,22 @@
 module.exports = (Discord, client) => {
   console.log(`${client.user.tag} is online!. \nEvents Running \nCommands Registered \nLoading Database...`);
+
+  const statusArray = [
+    `Prefix: $`,
+    `Type Command: $help`,
+    `Baking a cake`,
+    `Waking up our developers!`,
+    `Developers: Devnamyte#0001 and JUSTMRBHD#0001`,
+    `Over ${client.guilds.cache.size} servers!`,
+    `Over ${client.channels.cache.size} channels!`,
+    `Over ${client.users.cache.size} users!`
+  ];
+
+  let index = 0;
+  setInterval(() => {
+    if (index === statusArray.length) index = 0;
+    const status = statusArray[index];
+    client.user.setActivity(status, { type: "WATCHING" });
+    index++;
+  }, 15000);
 }
